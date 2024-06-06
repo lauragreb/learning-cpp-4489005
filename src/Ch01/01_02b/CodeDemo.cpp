4236146a57bf29b4da8146137dd67230c3a436ec
lauragreb <laura.greb@bayer.com>
2024-06-06 13:56:31 +0000

    output message

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,12 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hi there!" << std::endl;  //standard library::character output << "Message" << standard library::end line;
+
+    std::cout << std::endl << std::endl; 
+    return(0);
+}
